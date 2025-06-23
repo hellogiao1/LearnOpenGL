6cf5139311bf6b6dec43a360ca9fbec28feda290
hellogiao1 <71645914+hellogiao1@users.noreply.github.com>
2025-06-24 00:25:52 +0800

    只剔除正面
    glCullFace函数有三个可用的选项：
    
    GL_BACK：只剔除背向面。
    GL_FRONT：只剔除正向面。
    GL_FRONT_AND_BACK：剔除正向面和背向面。

--- LearnOpenGL/MainTest.cpp
+++ LearnOpenGL/MainTest.cpp
@@ -210,6 +210,7 @@ int main()
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     glEnable(GL_CULL_FACE);
+    glCullFace(GL_FRONT);
     
     // shader configuration
     // --------------------
